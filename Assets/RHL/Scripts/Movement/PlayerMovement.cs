2c1a106aa0e8e41ce4e0eea487a6fc0287d0c58c
Can Olcer <shafyy@gmail.com>
2019-01-20 20:55:38 -0800

    added left handedness to movement

--- Assets/RHL/Scripts/Movement/PlayerMovement.cs
+++ Assets/RHL/Scripts/Movement/PlayerMovement.cs
@@ -12,6 +12,18 @@ namespace RHL.Scripts.Movement {
 
     [SerializeField] private float walkingSpeed = 5.0f;
 
+    private static OVRInput.Controller Controller {
+      get {
+          OVRInput.Controller controller = OVRInput.GetConnectedControllers();
+          if ((controller & OVRInput.Controller.LTrackedRemote) == OVRInput.Controller.LTrackedRemote) {
+              return OVRInput.Controller.LTrackedRemote;
+          } else if ((controller & OVRInput.Controller.RTrackedRemote) ==  OVRInput.Controller.RTrackedRemote) {
+              return OVRInput.Controller.RTrackedRemote;
+          }
+          return OVRInput.GetActiveController();
+      }
+    }
+
     // we make this public because often times we want to access it from other classes
     // AND we want set it from the inspector
     public bool WalkingAllowed = true;
@@ -31,8 +43,7 @@ namespace RHL.Scripts.Movement {
   	
   	void Update () {
   		if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad) && WalkingAllowed) {
-        // this currently only works with the right hand controller
-        controllerRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+        controllerRotation = OVRInput.GetLocalControllerRotation(Controller);
 
         primayTouchpadPos = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
 
